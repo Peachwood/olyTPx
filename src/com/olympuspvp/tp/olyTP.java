589d3df1a4ed0c83819d2a3c28f435f48a229a1c
Noah Willcox <Peachwoodquimby@yahoo.com>
2012-12-31 23:28:59 -0500

    V1.1 Added Tri Teleportation

--- src/com/olympuspvp/tp/olyTP.java
+++ src/com/olympuspvp/tp/olyTP.java
@@ -13,15 +13,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class olyTP extends JavaPlugin{
 
 	final String tag = ChatColor.GOLD + "[" + ChatColor.YELLOW + "olyTP" + ChatColor.GOLD+"] " + ChatColor.YELLOW;
-	
+
 	@Override
 	public void onEnable(){
 		System.out.println("[olyTP] olyTP by willno123 (badass coderleetzors) has been enabled!");
 	}
-	
+
 	public boolean onCommand(CommandSender s, Command cmd, String c, String[] args){
 		if(s instanceof Player == false){
-			System.out.println("[olyTP] Go home console, you're drunk");
+			System.out.println("[olyTP] Go home console, you're drunk.");
 			return true;
 		}Player p = (Player)s;
 		if((p.isOp()==false)&&(p.hasPermission("olyTP.use")==false)){
@@ -31,7 +31,7 @@ public class olyTP extends JavaPlugin{
 			String to = args[0];
 			List<Player> matches = Bukkit.matchPlayer(to);
 			if(matches.size() !=1){
-				p.sendMessage(tag+"The player could not be found");
+				p.sendMessage(tag+"The player could not be found.");
 				return true;
 			}Player pto=matches.get(0);
 			if(p==pto){
@@ -39,10 +39,25 @@ public class olyTP extends JavaPlugin{
 				return true;
 			}p.teleport(pto, TeleportCause.PLUGIN);
 			p.sendMessage(tag + "Teleporting you to " + ChatColor.GOLD + pto.getName());
-			if(p.isOp() || p.hasPermission("olyTP.use")) pto.sendMessage(tag+ ChatColor.GOLD + p.getName() + " teleported to you.");
+			if(p.isOp() || p.hasPermission("olyTP.use")) pto.sendMessage(tag+ ChatColor.GOLD + p.getName() + ChatColor.YELLOW +  " teleported to you.");
 		}else if(args.length == 2){
-			//put code for teleporting people to other people
-			p.sendMessage(tag + "This command has not yet been implemented.");
+			String to = args[1];
+			String from = args[0];
+			List<Player> matches = Bukkit.matchPlayer(to);
+			if(matches.size() !=1){
+				p.sendMessage(tag + to + " could not be found.");
+				return true;
+			}
+			Player pto = matches.get(0);
+			matches = Bukkit.matchPlayer(from);
+			if(matches.size() !=1){
+				p.sendMessage(tag + from + " could not be found.");
+			}
+			Player pfrom = matches.get(0);
+			p.teleport(pto, TeleportCause.PLUGIN);
+			pfrom.sendMessage(tag + "Teleporting you to " + ChatColor.GOLD + pto.getName());
+			p.sendMessage(tag + "Teleporting " + ChatColor.GOLD + pfrom.getName() + ChatColor.YELLOW + " to " + ChatColor.GOLD + pto.getName());
+			pto.sendMessage(tag + ChatColor.GOLD + pfrom.getName() + ChatColor.YELLOW + " has been teleported to you.");
 			return true;
 		}return true;
 	}

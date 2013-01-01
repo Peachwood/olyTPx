ea49d00231404ded991fa811c8aaac2d46c3ce3a
CalDaBeast <cal@The-Pheonix.local>
2012-12-31 22:23:46 -0500

    v1.0.2 - Correcting Code Conventions

--- src/com/olympuspvp/tp/olyTP.java
+++ src/com/olympuspvp/tp/olyTP.java
@@ -13,6 +13,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class olyTP extends JavaPlugin{
 
 	final String tag = ChatColor.GOLD+"["+ChatColor.YELLOW+"olyTP"+ChatColor.GOLD+"] "+ChatColor.YELLOW;
+	
 	@Override
 	public void onEnable(){
 		System.out.println("[olyTP] Noah is a badass coderleetzors.");
@@ -22,9 +23,7 @@ public class olyTP extends JavaPlugin{
 		if(s instanceof Player == false){
 			System.out.println("[olyTP] Go home console, you're drunk");
 			return true;
-		}
-		
-		Player p = (Player)s;
+		}Player p = (Player)s;
 		if((p.isOp()==false)&&(p.hasPermission("olyTP.use")==false)){
 			p.sendMessage(tag+"You do not have permission to use this command.");
 		}
@@ -34,7 +33,6 @@ public class olyTP extends JavaPlugin{
 			if(matches.size() !=1){
 				p.sendMessage(tag+"The player could not be found");
 				return true;
-		
 			}
 			Player pto=matches.get(0);
 			if(p==pto){
@@ -44,10 +42,6 @@ public class olyTP extends JavaPlugin{
 			}
 			p.teleport(pto, TeleportCause.PLUGIN);
 			p.sendMessage(tag+"Teleporting you to "+ChatColor.GOLD+pto.getName());
-			
-		}
-		return true;
+		}return true;
 	}
-	
-	
 }
